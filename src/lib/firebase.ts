9d194f71cdf42ba32f59c9aaaa34ae15fb36543e
RoiSoyBoy <109469167+RoiSoyBoy@users.noreply.github.com>
2025-05-15 15:45:51 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth'; // Import getAuth
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCcGNgfhlKRKbLu_iHuIClQIi8iFjc-gJ4',
-  authDomain: 'chatbot-e17e5.firebaseapp.com',
-  projectId: 'chatbot-e17e5',
-  storageBucket: 'chatbot-e17e5.firebasestorage.app',
-  messagingSenderId: '645388218849',
-  appId: '1:645388218849:web:bbc6138ecd13c8ff31e2a0',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Initialize Firebase
